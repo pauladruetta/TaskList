8d60578fe8e8b2f8cb6e42cf52d304ee7f0de31a
Paula Druetta <druetta.paula@gmail.com>
2022-02-07 15:30:56 -0300

    Update app

---
+++ TaskList/src/app/components/add-task/add-task.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { UiService } from 'src/app/service/ui.service';
+import { AddTaskComponent } from './add-task.component';
+
+describe('AddTaskComponent', () => {
+  let component: AddTaskComponent;
+  let fixture: ComponentFixture<AddTaskComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ AddTaskComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AddTaskComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
--- TaskList/src/app/components/tasks/tasks.component.ts
+++ TaskList/src/app/components/tasks/tasks.component.ts
@@ -20,4 +20,25 @@ export class TasksComponent implements OnInit {
     ));
   }
 
+  deleteTask(task: Task){
+    this.taskService.deleteTask(task).subscribe(()=>[
+      this.tasks = this.tasks.filter((t) =>
+      {
+        return t.id != task.id
+      })
+    ])
+  }
+
+  toggleReminder(task: Task){
+    task.reminder = !task.reminder
+    this.taskService.updateTaskReminder(task).subscribe()
+  }
+
+  addTask(task:Task){
+    console.log(task);
+    this.taskService.addTask(task).subscribe((task)=> [
+      this.tasks.push(task)
+    ])
+  }
+
 }
